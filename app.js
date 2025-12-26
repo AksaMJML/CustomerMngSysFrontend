c78debcc0bdb63fd8f94a5aaee80e88cedff9e75
AksaMJML <akaks0186@gmail.com>
2025-12-26 22:32:14 +0530

    retrive the json from console

--- app.js
+++ app.js
@@ -3,18 +3,6 @@ async function loadCustomer() {
    const customers = await response.json();
    console.log(customers);
 
-   let body = '';
-
-   customers.forEach(customer => {
-    body += `<tr>
-                <td>${customer.id}</td>
-                <td>${customer.name}</td>
-                <td>${customer.address}</td>
-                <td>${customer.email}</td>
-                <td>${customer.salary}</td>
-            </tr>`;
-   });
-   
-   document.getElementById('tblCustomers').innerHTML = body;
 }
+
 loadCustomer();
