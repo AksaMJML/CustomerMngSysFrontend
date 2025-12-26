25deb334022d4fd212c12cbc4439898f981ad09d
AksaMJML <akaks0186@gmail.com>
2025-12-26 23:25:57 +0530

    viewed json json through table

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ async function loadCustomer() {
     const response = await fetch ('http://localhost:8080/get-customer');
    const customers = await response.json();
    console.log(customers);
+   
+   let body = "";
 
+   customers.forEach(customer => {
+         body += `<tr>
+                <td>${customer.id}</td>
+                <td>${customer.name}</td>
+                <td>${customer.address}</td>
+                <td>${customer.email}</td>
+                <td>${customer.salary}</td>
+            </tr>`;
+   });
+
+   document.getElementById('tblCustomers').innerHTML = body;
+       
 }
 
 loadCustomer();
